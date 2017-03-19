bb2457ebde420ca3b302c7dda059d71ce3581571
Bhasin, Krishan <Krishan.Bhasin@capitalone.com>
2017-03-19 03:49:10 +0000

    v1.2

--- functions.py
+++ functions.py
@@ -8,22 +8,32 @@ import pandas as pd
 
 
 
-
 def collectAlexaFeatures(featureUrl, payload, headers):
     r = requests.post(
         featureUrl,
         data=json.dumps(payload),
         headers=headers
         )
-    alexa =  json.loads(str(r.text))
+    alexa = json.loads(str(r.text))
 
     alexa_engagement = alexa['engagement']
     alexa_countryRank = alexa['countryRank']
     alexa_globalRank = alexa['globalRank']
 
     # intentionally skipping countryrank for now
-    alexa_dict = {'url':payload['url'],'dailyTimeOnSite': alexa_engagement['dailyTimeOnSite'],'bounceRate':alexa_engagement['bounceRate'],'dailyPageViewPerVisitor':alexa_engagement['dailyPageViewPerVisitor'],'globalRank': alexa_globalRank}
+    alexa_dict = {'url': payload['url'], 'dailyTimeOnSite': alexa_engagement['dailyTimeOnSite'], 'bounceRate': alexa_engagement['bounceRate'], 'dailyPageViewPerVisitor': alexa_engagement['dailyPageViewPerVisitor'], 'globalRank': alexa_globalRank}
     alexa_df = pd.DataFrame(alexa_dict, index=[0])
-    
-    #return the dataframe
+
+    # return the dataframe
     return alexa_df
+
+
+def collectNumberOfAdverts(featureUrl, payload, headers):
+    r = requests.post(
+        featureUrl,
+        data=json.dumps(payload),
+        headers=headers
+        )
+    ads = json.loads(str(r.text))
+    print(ads)
+
--- main.py
+++ main.py
@@ -10,31 +10,33 @@ from functions import *
 
 app = Flask(__name__)
 
+ADCOUNT = "https://adshunter.herokuapp.com"
+ALEXA = "https://alexar.herokuapp.com"
+DEADLINKS = "https://broken-links-service.herokuapp.com/"
+QUESTION = 'locn'
 
-@app.route("/", methods = ['POST'])
+
+@app.route("/", methods=['POST'])
 def hello():
-    url =  json.loads(request.data)['url']
+    url = json.loads(request.data)['url']
     page = requests.get(url)
-    tree = html.fromstring(page.content)
     payload = {
 #        "HTML":page.text,
-        "url":str(url)
+        "url": str(url)
         }
 
-    headers = {"Content-Type":"application/json"}
+    headers = {"Content-Type": "application/json"}
 
     # alexa data frame
-    alexa = collectAlexaFeatures("http://localhost:3000", payload, headers)
-
-    #join all data frames together, using URL as the key (or just colbind...)
+    alexa = collectAlexaFeatures(ALEXA, payload, headers)
+    ads = collectNumberOfAdverts(ADCOUNT, payload, headers)
+    # join all data frames together, using URL as the key (or just colbind...)
 
     # put data through predefined scorecard
-    
+
     score = 9
 
-    return json.dumps({"assignedScore":score})
+    return json.dumps({"assignedScore": score})
 
 if __name__ == "__main__":
     app.run(debug=True)
-    # title = tree.xpath('//title/text()')
-    # header = tree.xpath('//h1/text()')
