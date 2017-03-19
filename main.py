3aa9016ca51321b9ee1f41705007982d58a29c6e
Bhasin, Krishan <Krishan.Bhasin@capitalone.com>
2017-03-19 11:03:45 +0000

    V1 OF THE MACHINE-LEARNED MODELgit add .git add .git add .git add .111

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from flask import request
 import json
 import pandas as pd
 import numpy as np
+from sklearn.externals import joblib
+
 # user defined functions
 from functions import *
 from scoreout import *
+
+model = joblib.load('fakeNews.pkl')
 
 app = Flask(__name__)
 
@@ -20,6 +24,10 @@ SELFLINKS = 'https://self-link-service.herokuapp.com/'
 @app.route("/", methods=['POST'])
 def hello():
     url = json.loads(request.data)['url']
+
+    print "post recieved for:"
+    print url
+    
     page = requests.get(url)
     payload_heavy = {
         "html": page.text,
@@ -30,17 +38,17 @@ def hello():
         }
     headers = {"Content-Type": "application/json"}
 
+    print "sending first request"
     brokenlinks = collectBrokenLinks(DEADLINKS, payload_heavy, headers)
+    print "broken links done"
     alexa = collectAlexaFeatures(ALEXA, payload_light, headers)
+    print "alexa done"
     selflinks = collectSelflinks(SELFLINKS, payload_heavy, headers)
+    print "self links done"
 
-
-    # join all data frames together, using URL as the key (or just colbind...)
     all_features = brokenlinks.merge(alexa, how='inner', on='url').merge(selflinks, how='inner', on='url')
 
-    score = int(scoreArticle(all_features))
-
-    print(score)
+    score = scoreArticle(all_features, model)
 
     return json.dumps({"assignedScore": score})
 
@@ -58,14 +66,10 @@ def train():
         }
     headers = {"Content-Type": "application/json"}
 
-
     brokenlinks = collectBrokenLinks(DEADLINKS, payload_heavy, headers)
     alexa = collectAlexaFeatures(ALEXA, payload_light, headers)
     selflinks = collectSelflinks(SELFLINKS, payload_heavy, headers)
 
-
-
-    # could use pd.concat() to make this cleaner later.
     all_features = brokenlinks.merge(alexa, how='inner', on='url').merge(selflinks, how='inner', on='url')
 
     return all_features.to_json(orient='records')
--- scoreout.py
+++ scoreout.py
@@ -1,17 +1,25 @@
 import pandas as pd
 import numpy as np
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
 
 
-def scoreArticle(df):
+def scoreArticle(df, model):
+
+    print "scoring model"
 
     # Drop URL in order to allow the cast to numeric to *not* fail
     df = df.drop('url', 1)
 
-    # print "starting scoreout"
-    # print(df)
-    # print "---------------"
-    # df.apply(pd.to_numeric)
-    # print "---------------"
-    # print(df)
+    probs = model.predict_proba(df)
+    print probs
 
-    return df.sum(axis=1)%100
+    probability_of_fake = probs[0][0]*100
+
+    # Probability of FAKE NEWS is first in the array (element 0)
+    # return probs
+
+    print probability_of_fake
+
+    return int(probability_of_fake)
