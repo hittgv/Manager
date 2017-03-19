328e85e18d46df72859237a60847e9b3a38b63e2
Bhasin, Krishan <Krishan.Bhasin@capitalone.com>
2017-03-19 08:40:00 +0000

    remove prints for speed

--- scoreout.py
+++ scoreout.py
@@ -7,11 +7,11 @@ def scoreArticle(df):
     # Drop URL in order to allow the cast to numeric to *not* fail
     df = df.drop('url', 1)
 
-    print "starting scoreout"
-    print(df)
-    print "---------------"
-    df.apply(pd.to_numeric)
-    print "---------------"
-    print(df)
+    # print "starting scoreout"
+    # print(df)
+    # print "---------------"
+    # df.apply(pd.to_numeric)
+    # print "---------------"
+    # print(df)
 
     return df.sum(axis=1)%100
